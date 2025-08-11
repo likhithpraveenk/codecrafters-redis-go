c69f9af77ef1c990345c6c34506fa2f388c98285
likhithpraveenk <lpkatlagunta@gmail.com>
2025-08-11 23:13:31 +0530

    WY1

--- app/main.go
+++ app/main.go
@@ -12,11 +12,30 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	defer l.Close()
+	fmt.Println("Listening on port 6379")
 
-	conn.Write([]byte("+PONG\r\n"))
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	for {
+		_, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			return
+		}
+	}
 }
