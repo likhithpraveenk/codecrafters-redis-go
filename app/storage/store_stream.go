69334cb5d0e578f49bd35586215c2033668f2d06
likhithpraveenk <lpkatlagunta@gmail.com>
2025-08-13 20:49:36 +0530

    xadd validation

--- app/storage/store_stream.go
+++ app/storage/store_stream.go
@@ -2,6 +2,8 @@ package store
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -16,13 +18,88 @@ func XAdd(key, id string, fields map[string]string) (string, error) {
 		return "", fmt.Errorf("WRONGTYPE Operation against a key")
 	}
 	stream := it.value.([]StreamEntry)
-
-	if id == "*" {
-		ms := time.Now().UnixNano()
-		id = fmt.Sprintf("%d-0", ms)
+	var lastID string
+	if len(stream) > 0 {
+		lastID = stream[len(stream)-1].ID
 	}
-	stream = append(stream, StreamEntry{ID: id, Fields: fields})
+	newId, err := validateAndGenerateStreamID(id, lastID)
+	if err != nil {
+		return "", err
+	}
+	stream = append(stream, StreamEntry{ID: newId, Fields: fields})
 	it.value = stream
 	store[key] = it
-	return id, nil
+	return newId, nil
+}
+
+func validateAndGenerateStreamID(id string, lastID string) (string, error) {
+	lastMS, lastSeq := parseIDParts(lastID)
+	switch {
+	case id == "*":
+		msInt := time.Now().UnixNano() / 1e6
+		ms := fmt.Sprintf("%d", msInt)
+		var seq int64 = 0
+		if lastID != "" {
+			parts := strings.Split(lastID, "-")
+			if len(parts) == 2 && parts[0] == ms {
+				lastSeq, _ := strconv.ParseInt(parts[1], 10, 64)
+				seq = lastSeq + 1
+			}
+		}
+		return fmt.Sprintf("%s-%d", ms, seq), nil
+	case strings.HasSuffix("-*", id):
+		ms := strings.TrimSuffix("-*", id)
+		msInt, err := strconv.ParseInt(ms, 10, 64)
+		if err != nil || msInt < 0 {
+			return "", fmt.Errorf("invalid stream ID specified")
+		}
+		var seq int64 = 0
+		if msInt == lastMS {
+			seq = lastSeq + 1
+		}
+		if !isIDGreater(msInt, seq, lastMS, lastSeq) {
+			return "", fmt.Errorf("ID specified is equal or smaller than the target stream top item")
+		}
+		return fmt.Sprintf("%d-%d", msInt, seq), nil
+	default:
+		parts := strings.Split(id, "-")
+		if len(parts) != 2 {
+			return "", fmt.Errorf("invalid stream ID format")
+		}
+		ms, err1 := strconv.ParseInt(parts[0], 10, 64)
+		seq, err2 := strconv.ParseInt(parts[1], 10, 64)
+		if err1 != nil || err2 != nil || ms < 0 || seq < 0 || (ms == 0 && seq == 0) {
+			errMsg := "The ID specified in XADD must be greater than 0-0"
+			return "", fmt.Errorf("%s", errMsg)
+		}
+		if !isIDGreater(ms, seq, lastMS, lastSeq) {
+			errMsg := "The ID specified in XADD is equal or smaller than the target stream top item"
+			return "", fmt.Errorf("%s", errMsg)
+		}
+		return id, nil
+	}
+}
+
+func parseIDParts(id string) (int64, int64) {
+	if id == "" {
+		return 0, 0
+	}
+	parts := strings.Split(id, "-")
+	if len(parts) != 2 {
+		return 0, 0
+	}
+
+	ms, _ := strconv.ParseInt(parts[0], 10, 64)
+	seq, _ := strconv.ParseInt(parts[1], 10, 64)
+	return ms, seq
+}
+
+func isIDGreater(ms, seq, lastMs, lastSeq int64) bool {
+	if ms > lastMs {
+		return true
+	}
+	if ms == lastMs && seq > lastSeq {
+		return true
+	}
+	return false
 }
